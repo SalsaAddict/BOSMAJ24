e44a24a176a15055f70b09613c13b5a085a514cd
Pierre Henry <pierre.henry@salsaaddict.com>
2024-10-04 09:19:06 +0100

    environments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from './auth.service';
+import { environment } from '../environments/environment';
 import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/BOSMAJ24' }],
+  providers: [{ provide: APP_BASE_HREF, useValue: environment.appBaseUrl }],
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  appBaseUrl: '/BOSMAJ24'
+};
